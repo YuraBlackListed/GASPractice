6188eb5f5dc4a969fdd5d08d2111163a94102c5e
BlackListed <yura.riddig@gmail.com>
2024-03-28 16:09:17 +0200

    Added VisualStudio sln

---
+++ Source/GASPractice/Private/AnEmptyClass.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AnEmptyClass.h"
+
+AnEmptyClass::AnEmptyClass()
+{
+}
+
+AnEmptyClass::~AnEmptyClass()
+{
+}
